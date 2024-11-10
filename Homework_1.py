f974cd437b8c317c56af57f63c18b0a650e10240
ana003m <138608863+ana003m@users.noreply.github.com>
2024-11-10 23:48:10 +0100

    Update Homework_1.py

--- Homework_1.py
+++ Homework_1.py
@@ -7,10 +7,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import datetime
 import math
-from selenium.webdriver.common.action_chains import ActionChains
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
-url = "https://www.mse.mk/mk/stats/symbolhistory/REPl"
+url = "https://www.mse.mk/mk/stats/symbolhistory/REPL"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
 def get_dropdown_options(soup):
     select_element = soup.find('select', id='Code')
@@ -18,31 +20,32 @@ def get_dropdown_options(soup):
     return [option.get('value') for option in options]
 
 def filter_options(options):
-    return [opt for opt in options if not any(char.isdigit() for char in opt)]
+    return [code for code in options if not any(char.isdigit() for char in code)]
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
 options = get_dropdown_options(soup)
-options = filter_options(options)
+codes = filter_options(options)
 
-fname = os.getcwd() + "hist.csv"
-if os.path.isfile(fname):
-    print("Database exists")
+fname = os.path.join(os.getcwd(), "hist.csv")
+
+if not os.path.isfile(fname):
+    df = pd.DataFrame(columns=["Код", "Датум", "Цена", "Обем"]) 
+    df.to_csv(fname, sep=';', index=False)
+    print("Created <3")
 else:
-    with open(mode="w+", file="./hist.csv") as file:
-        print("Created ")
-
-
-df = pd.read_csv("./hist.csv", sep=';')
+   
+    if os.stat(fname).st_size == 0:
+        print("CSV file is empty, initializing with columns.")
+        df = pd.DataFrame(columns=["Код", "Датум", "Цена", "Обем"])  
+    else:
+        df = pd.read_csv(fname, sep=';')
 
 driver = webdriver.Firefox()
-
 driver.get(url)
 
-for option in options:
+for code in codes:
     select_el = driver.find_element(By.ID, 'Code')
     select = Select(select_el)
-    select.select_by_visible_text(option)
+    select.select_by_visible_text(code)
 
     button_element = driver.find_element(By.CLASS_NAME, 'btn-primary-sm')
     button_element.click()
@@ -50,26 +53,30 @@ for option in options:
     table = driver.find_element(By.ID, 'resultsTable')
     rows = table.find_elements(By.TAG_NAME, "tr")
 
-    biggest_date = df[df['Код'] == option]['Датум'].max() or datetime.datetime(1925, 1, 1)
+    biggest_date = df[df['Код'] == code]['Датум'].max()
+    biggest_date = datetime.datetime(1925, 1, 1) if pd.isna(biggest_date) else datetime.datetime.strptime(biggest_date, "%d-%m-%Y")
 
     for row in rows:
         cells = row.find_elements(By.TAG_NAME, "td")
 
         if len(cells) > 0:
             row_date_str = cells[0].text.strip()
-
+        
             try:
                 row_date = datetime.datetime.strptime(row_date_str.replace('.', '-'), "%d-%m-%Y")
                 
                 if row_date > biggest_date:
-                    row_data = f'{option}; ' + '; '.join(cell.text for cell in cells)
+                    row_data = f'{code}; ' + '; '.join(cell.text for cell in cells)
                     row_values = row_data.split("; ")
                     new_row = pd.DataFrame([row_values], columns=df.columns)
+
                     df = pd.concat([df, new_row], ignore_index=True)
         
             except ValueError:
                 print(f"Skipping row with invalid date: {row_date_str}")
 
 driver.quit()
+
+biggest_date = df[df['Код'] == codes[1]]['Датум'].max()
 
 df
